d96de04e18ad8aef34882ae444487aa5d267185d
Fedor Garin <fedor.garin@gmail.com>
2015-11-22 20:15:34 -0500

    uncomment db

--- alma/settings/base.py
+++ alma/settings/base.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'alma.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
