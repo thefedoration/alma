f56a319ba13b9a07870871c341f9c50562175b97
Fedor Garin <fedor.garin@gmail.com>
2015-11-22 20:35:46 -0500

    fuck it

--- alma/settings/production.py
+++ alma/settings/production.py
@@ -9,5 +9,5 @@ DEBUG = False
 import dj_database_url
 DATABASES['default'] =  {
     'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'prod_db.sqlite3'),
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
