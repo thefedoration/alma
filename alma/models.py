c3cf1d961d3b0fecc60de993206fe74b93d4b214
Fedor Garin <fedor.garin@gmail.com>
2015-11-27 16:22:31 -0500

    email and google site verification

--- alma/models.py
+++ alma/models.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+
+import os
+import sys
+import sendgrid
 
 
 class Lead(models.Model):
@@ -16,3 +22,34 @@ class Lead(models.Model):
 
     def __unicode__(self):
         return "%s - %s" % (self.name, self.email)
+
+
+"""
+SIGNALS FOR CREATING LEADS
+"""
+
+@receiver(post_save, sender=Lead)
+def send_lead_email(sender, instance, created, *args, **kwargs):
+    if created:
+        print "created!!!"
+        sys.stdout.flush()
+        print instance
+        sys.stdout.flush()
+
+        sg = sendgrid.SendGridClient(os.environ.get('SENDGRID_USERNAME'), os.environ.get('SENDGRID_PASSWORD'))
+
+        message = sendgrid.Mail()
+        message.add_to('Fedor Garin <fedor.garin@gmail.com>')
+        message.set_subject('New Almify lead - %s' % instance.email)
+        message.set_text('\n \
+            Name - %s \n \
+            Email - %s \n \
+            Organization - %s \n \
+            Description - %s \n \
+            Time - %s \n \
+        ' % (instance.name, instance.email, instance.organization, instance.description or "", instance.create_date))
+        message.set_from('Almify Leads <leads@almify.com>')
+        status, msg = sg.send(message)
+
+
+
--- alma/views.py
+++ alma/views.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from django.shortcuts import render, redirect
+import sys
 
 from alma.models import Lead
 
